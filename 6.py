5a0df05ba279ebe67b92cd2d32cdfbcd95bdf17c
pony-8 <2604665482@qq.com>
2020-05-26 09:48:53 +0800

    第六天

---
+++ 6.py
@@ -0,0 +1,21 @@
+'''
+
+(1)介绍下Python中的map函数？
+map函数会根据提供的函数对指定序列做映射。其中第一个参数 function 以参数序列中的每一个元素调用 function 函数，
+返回包含每次 function 函数返回值的新列表。
+
+(2)在Django框架中如何使用会话？
+Django提供了一个会话，允许每个站点访问者存储和检索数据。Django通过在客户端放置会话ID cookie并在服务器端存储所有相关数据来抽象发送和接收cookie的过程。
+
+(3)提到Django，Pyramid和Flask之间的差异
+Flask是一个“微框架”，主要用于具有更简单要求的小型应用程序。在烧瓶中，您必须使用外部库。Flask已准备好使用。
+Pyramid适用于大型应用程序。它提供了灵活性，并允许开发人员为他们的项目使用正确的工具。开发人员可以选择数据库，URL结构，模板样式等。金字塔是可配置的。
+Django也可以像Pyramid一样用于更大的应用程序。它包括一个ORM。
+
+(4)什么是Python补丁？
+在Python中，补丁是指在运行时通过类外部的函数对类或模块进行动态修改。
+
+(5)什么是Python库？
+Python库是Python包的集合。一些主要使用的python库是Numpy、Pandas、Matplotlib和Scikit-learn等等。
+
+'''
